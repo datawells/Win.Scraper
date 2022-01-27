7ac40529ffcac8b08000041beaa0da09021ffbe8
inwells <accounts@straiva.com>
2022-01-27 17:22:12 -0600

    Successful db test for posts

--- TablePop.sql
+++ TablePop.sql
@@ -1,86 +1,101 @@
+use test_db;
+CREATE TABLE communities (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `community` VARCHAR(25),
+    PRIMARY KEY (`id`)
+);
 CREATE TABLE users (
-    id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    user VARCHAR(25),
-    PRIMARY KEY (id)
-)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user` VARCHAR(25),
+    `community` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`community`)
+        REFERENCES communities(`id`)
+        ON DELETE CASCADE
+);
 CREATE TABLE posts (
-    id INT NOT NULL UNIQUE,
-    uuid VARCHAR(10) NOT NULL UNIQUE,
-    preview varchar(65535),
-    is_locked BOOLEAN,
-    tweet_id VARCHAR(50),
-    sticky_comment INT,
-    link varchar(65535),
-    author_flair_class VARCHAR(200),
-    is_video_mp4 BOOLEAN,
-    is_removed BOOLEAN,
-    title VARCHAR,
-    type VARCHAR(10),
-    last_comment_created INT,
-    content TEXT,
-    score_down INT,
-    author_flair_text VARCHAR(65535),
-    is_twitter BOOLEAN,
-    is_admin BOOLEAN,
-    score INT,
-    suggested_sort INT,
-    is_stickied BOOLEAN,
-    is_nsfw BOOLEAN,
-    post_flair_class VARCHAR(50),
-    is_deleted BOOLEAN,
-    is_image BOOLEAN,
-    comments INT,
-    author INT,
-    created INT,
-    score_up INT,
-    is_edited BOOLEAN,
-    community VARCHAR(25),
-    is_moderator BOOLEAN,
-    last_comment_author INT,
-    is_video BOOLEAN,
-    video_link varchar(65535),
-    is_new_user BOOLEAN,
-    vote_state INT,
-    domain VARCHAR(255),
-    post_flair_text VARCHAR(50),
-    is_crosspost BOOLEAN,
-    PRIMARY KEY (id)
-    FOREIGN KEY (author)
-        REFERENCES users(id)
+    `id` INT NOT NULL UNIQUE,
+    `uuid` VARCHAR(10) NOT NULL UNIQUE,
+    `preview` BLOB,
+    `is_locked` BOOLEAN NOT NULL,
+    `is_twitter` BOOLEAN NOT NULL,
+    `tweet_id` VARCHAR(50),
+    `sticky_comment` INT NOT NULL,
+    `link` BLOB,
+    `domain` VARCHAR(255),
+    `author_flair_class` VARCHAR(200),
+    `is_video_mp4` BOOLEAN NOT NULL,
+    `is_removed` BOOLEAN NOT NULL,
+    `title` TEXT,
+    `type` VARCHAR(10) NOT NULL,
+    `content` TEXT,
+    `score_up` INT NOT NULL,
+    `score_down` INT NOT NULL,
+    `score` INT NOT NULL,
+    `author_flair_text` BLOB,
+    `is_admin` BOOLEAN NOT NULL,
+    `suggested_sort` INT NOT NULL,
+    `is_stickied` BOOLEAN NOT NULL,
+    `is_nsfw` BOOLEAN NOT NULL,
+    `post_flair_class` VARCHAR(50),
+    `is_deleted` BOOLEAN NOT NULL,
+    `is_image` BOOLEAN NOT NULL,
+    `comments` INT,
+    `author` INT NOT NULL,
+    `created` BIGINT NOT NULL,
+    `is_edited` BOOLEAN NOT NULL,
+    `community` INT NOT NULL,
+    `is_moderator` BOOLEAN NOT NULL,
+    `last_comment_author` INT,
+    `last_comment_created` BIGINT,
+    `is_video` BOOLEAN NOT NULL,
+    `video_link` BLOB,
+    `is_new_user` BOOLEAN NOT NULL,
+    `vote_state` INT NOT NULL,
+    `post_flair_text` VARCHAR(50),
+    `is_crosspost` BOOLEAN NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`author`)
+        REFERENCES users(`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`last_comment_author`)
+        REFERENCES users(`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`community`)
+        REFERENCES communities(`id`)
         ON DELETE CASCADE
-    FOREIGN KEY (last_comment_author)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+);
 CREATE TABLE comments (
-    id INT NOT NULL UNIQUE,
-    uuid VARCHAR(10) NOT NULL UNIQUE,
-    parent_id INT NOT NULL,
-    post_uuid VARCHAR(10) NOT NULL,
-    is_removed BOOLEAN,
-    content TEXT,
-    score_down INT,
-    is_admin BOOLEAN,
-    score INT,
-    is_stickied BOOLEAN,
-    is_deleted BOOLEAN,
-    author INT,
-    created INT,
-    score_up INT,
-    comment_parent_id INT,
-    is_edited BOOLEAN,
-    community VARCHAR,
-    is_moderator BOOLEAN,
-    is_new_user BOOLEAN,
-    vote_state INT,
-    PRIMARY KEY (id)
-    INDEX par_ind (parent_id,post_uuid),
-    INDEX (comment_parent_id)
-    FOREIGN KEY (author)
-        REFERENCES Users(id)
+    `id` INT NOT NULL UNIQUE,
+    `uuid` VARCHAR(10) NOT NULL UNIQUE,
+    `parent_id` INT NOT NULL,
+    `post_uuid` VARCHAR(10) NOT NULL,
+    `is_removed` BOOLEAN NOT NULL,
+    `content` TEXT,
+    `score_down` INT NOT NULL,
+    `is_admin` BOOLEAN NOT NULL,
+    `score` INT NOT NULL,
+    `is_stickied` BOOLEAN NOT NULL,
+    `is_deleted` BOOLEAN NOT NULL,
+    `author` INT NOT NULL,
+    `created` BIGINT NOT NULL,
+    `score_up` INT NOT NULL,
+    `comment_parent_id` INT,
+    `is_edited` BOOLEAN NOT NULL,
+    `community` INT NOT NULL,
+    `is_moderator` BOOLEAN NOT NULL,
+    `is_new_user` BOOLEAN NOT NULL,
+    `vote_state` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX par_ind (`parent_id`,`post_uuid`),
+    INDEX (`comment_parent_id`),
+    FOREIGN KEY (`author`)
+        REFERENCES users(`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`parent_id`)
+        REFERENCES posts(`id`)
+        ON DELETE CASCADE,
+    FOREIGN KEY (`community`)
+        REFERENCES communities(`id`)
         ON DELETE CASCADE
-    FOREIGN KEY (parent_id)
-        REFERENCES posts(id)
-        ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
-GO
+);
--- testscrape.py
+++ testscrape.py
@@ -1,39 +1,87 @@
 import json
 import requests
+import mysql.connector
 from tabulate import tabulate
 import numpy as np
 
-url = "https://communities.win/api/v2/post/newv2.json?community=technology"
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  port="3306",
+  user="test",
+  password="test_pass",
+  database="test_db",
+  auth_plugin="mysql_native_password")
+
+cursor = mydb.cursor(buffered=True)
+
+#values for community lookup
+community = "technology"
+phase1 = f"https://communities.win/api/v2/post/newv2.json?community={community}"
+
+#populate community / retrieve community ID
+cursor.execute("SELECT id FROM communities WHERE community = %s", (community,))
+cids = cursor.fetchone()
+
+if cids==None:
+    try:
+        insert = "INSERT INTO communities (id, community) VALUES (%s, %s)"
+        ival = ("0", community)
+        cursor.execute(insert, ival)
+        mydb.commit()
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+    cursor.execute("SELECT id FROM communities WHERE community = %s", (community,))
+    cids = cursor.fetchone()
+for cid in cids:
+    print(cid)
+    cid = cid
+
 global file
 file = "D:\stuff2.txt"
 sheet = [["id", "title", "author", "comments", "lastcomment", "lca"]]
 
+#append posts - aposts not used
 def appenddict(aposts,items):
     itemlist = items[1:]
     for x in itemlist:
         posts.append(x)
 
-def formsheet(postlist):
+#inserts posts into DB
+def insertposts(postlist):
     a_file = open(file, "w")
     json.dump(postlist,a_file)
     a_file.close()
     for x in postlist:
-        print(x['title'])
-        id = x['id']
-        title = x['title']
-        author = x['author']
-        comments = x['comments']
-        lastcomment = x['last_comment_created']
-        lca = x['last_comment_author']
-        sheet.append([id, title, author, comments, lastcomment, lca])
-    return sheet        
+        #print(x['title'])
 
+        x['author'] = useridlookup(x['author'])
+        if x['last_comment_author']:
+            x['last_comment_author'] = useridlookup(x['last_comment_author'])
+        else:
+            #TO DO seed null user on table creation
+            x['last_comment_author'] = "26"
+        try:
+            c = len(x)
+            s = "%s, " * c
+            s = s.rstrip(', ')
+            for v in x:
+                if x[v]==None:
+                    x[v] = "NULL"
+            insert = f"INSERT INTO posts (id, uuid, preview, is_locked, is_twitter, tweet_id, sticky_comment, link, domain, author_flair_class, is_video_mp4, is_removed, title, type, content, score_up, score_down, score, author_flair_text, is_admin, suggested_sort, is_stickied, is_nsfw, post_flair_class, is_deleted, is_image, comments, author, created, is_edited, community, is_moderator, last_comment_author, last_comment_created, is_video, video_link, is_new_user, vote_state, post_flair_text, is_crosspost) VALUES ({s})"
+            ival = (x['id'], x['uuid'], x['preview'], int(x['is_locked']), x['is_twitter'], x['tweet_id'], x['sticky_comment'], x['link'], x['domain'], x['author_flair_class'], int(x['is_video_mp4']), int(x['is_removed']), x['title'], x['type'], x['content'], x['score_up'], x['score_down'], x['score'], x['author_flair_text'], int(x['is_admin']), x['suggested_sort'], int(x['is_stickied']), int(x['is_nsfw']), x['post_flair_class'], int(x['is_deleted']), int(x['is_image']), x['comments'], x['author'], x['created'], int(x['is_edited']), cid, int(x['is_moderator']), x['last_comment_author'], x['last_comment_created'], int(x['is_video']), x['video_link'], int(x['is_new_user']), x['vote_state'], x['post_flair_text'], x['is_crosspost'])
+            cursor.execute(insert, ival)
+            mydb.commit()
+        except mysql.connector.Error as error:
+            print("Failed to insert into MySQL table {}".format(error))
+    # return sheet        
+
+#aggregates posts by loading json files and removing the duplicate
 def uuid_iteration(uposts):
     fpost = uposts[0]['uuid']
     lpost = uposts[-1]['uuid']
     print(lpost)
     while lpost != fpost:
-        iurl = f"{url}&from={lpost}"
+        iurl = f"{phase1}&from={lpost}"
         print(iurl)
         irequest = requests.get(iurl)
         idata = irequest.json()
@@ -45,13 +93,32 @@ def uuid_iteration(uposts):
             break
         appenddict(posts,iposts)
 
-request = requests.get(url)
+#populate user / search lookup ID
+def useridlookup(user):
+    cursor.execute("SELECT id FROM users WHERE user = %s", (user,))
+    r = cursor.fetchone()
+    if r==None:
+        try:
+            insert = "INSERT INTO users (id, user, community) VALUES (%s, %s, %s)"
+            ival = ("0", user, cid)
+            cursor.execute(insert, ival)
+            mydb.commit()
+        except mysql.connector.Error as error:
+            print("Failed to insert into MySQL table {}".format(error))
+        cursor.execute("SELECT id FROM users WHERE user = %s", (user,))
+        r = cursor.fetchone()
+    for f in r:
+        id = f
+    return id
+
+request = requests.get(phase1)
 data = request.json()
 global posts
 posts = data["posts"]
- 
-uuid_iteration(posts)
 
-formsheet(posts)
-print(tabulate(sheet))
+#Disable for testing. 
+#uuid_iteration(posts)
+
+insertposts(posts)
+#print(tabulate(sheet))
 print(len(sheet))
